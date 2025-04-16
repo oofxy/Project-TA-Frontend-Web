b8ad1b103f346874b53b0ca92d412ed43ddb4f74
WHOISCOOKIES <alvianputraleksana@gmail.com>
2025-04-16 12:39:28 +0700

    feat: [AP] implement data register user page design

---
+++ app/(root)/admin/register-user/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Table from "./table";
+import { dataRegisterUser } from "./columns";
+
+const DataRegisterUser = () => {
+  return (
+    <div className="w-full h-full bg-[#CDF9EF] rounded-3xl p-6">
+      <Table />
+    </div>
+  );
+};
+
+export default DataRegisterUser;
