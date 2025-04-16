c7da86710e2a7b34d927c9de277ad34a8e26e98c
WHOISCOOKIES <alvianputraleksana@gmail.com>
2025-04-16 12:04:42 +0700

    feat: [AP] implement admin data karyawan page design

---
+++ app/(root)/admin/data-karyawan/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Table from "./table";
+
+const DataKaryawan = () => {
+  return (
+    <div className="w-full h-full bg-[#CDF9EF] rounded-3xl p-6">
+      <Table />
+    </div>
+  );
+};
+
+export default DataKaryawan;
